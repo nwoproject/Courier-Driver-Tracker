8471f0d7995c1a2b3d5af75a17412bd6453a24d3
BrendanJvR <brendanjansenvanrensburg@gmail.com>
2020-05-27 15:20:45 +0200

    Initial commit with framework

---
+++ courier_driver_tracker/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CourierDriverTracker());
+}
+
+class CourierDriverTracker extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text(''),);
+  }
+}
