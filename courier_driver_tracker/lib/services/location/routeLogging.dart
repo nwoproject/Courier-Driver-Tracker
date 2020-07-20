965ee9fb16a947c7a0f30e61f63f4bc5ef5db011
Ryan <u15016880@tuks.co.za>
2020-07-20 16:15:06 +0200

    Added read and write file permission request.

--- courier_driver_tracker/lib/services/location/routeLogging.dart
+++ courier_driver_tracker/lib/services/location/routeLogging.dart
@@ -1,34 +1,26 @@
 import 'dart:async';
 import 'dart:io';
+import 'package:courier_driver_tracker/services/location/TrackingData.dart';
+import 'package:courier_driver_tracker/services/location/location_service.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:permissions_plugin/permissions_plugin.dart';
 
-// This class is to handle the file operations for route logging.
-class routeLogging{
+class RouteLogging{
+  TrackingData location;
 
-  static void getPermission() async{
-    Map<Permission, PermissionState> permission = await PermissionsPlugin
-        .requestPermissions([
-      Permission.READ_EXTERNAL_STORAGE,
-      Permission.WRITE_EXTERNAL_STORAGE
-    ]);
-  }
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
 
-    //gets the correct directory path to store the file.
-   static Future<String> get localPath async {
-    final directory = await getExternalStorageDirectory();
-    getPermission();
     return directory.path;
   }
-  //returns the file where the location data is written to.
-  static Future<File> get localFile async {
-    final path = await localPath;
-    return File('$path/Download/test.txt');
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/test.txt');
   }
 
   static Future<String> readFileContents() async {
     try {
-      final file = await localFile;
+      final file = await _localFile;
 
       // Read the file
       String contents = await file.readAsString();
@@ -41,10 +33,11 @@ class routeLogging{
   }
 
   static Future<File> writeToFile(String data) async {
-    final file = await localFile;
+    final file = await _localFile;
 
     // Write the file
     return file.writeAsString('data');
   }
 }
 
+}
