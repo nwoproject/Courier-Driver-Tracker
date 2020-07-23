1c9f39a1ff55e8d42b5c97b023bfeb616a6d0084
Ryan <u15016880@tuks.co.za>
2020-07-23 15:11:31 +0200

    Auto stash before merge of "development" and "origin/development"

---
+++ courier_driver_tracker/test/Route_logging_test.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:courier_driver_tracker/services/location/RouteLogging.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:path_provider/path_provider.dart';
+
+
+void main() {
+  group('PathProvider', () {
+    TestWidgetsFlutterBinding.ensureInitialized();
+
+    test('Local path should contain the path.', () async*{
+      bool created = false;
+      String path = RouteLogging.localPath.toString();
+      if (path.length > 0)
+        {
+          created = true;
+        }
+      else
+        created = false;
+      expect(created, true);
+    });
+
+    test('Local file should contain the file.', () async*{
+      bool created = false;
+      String file = RouteLogging.localFile.toString();
+      if (file.length > 0)
+      {
+        created = true;
+      }
+      else
+        created = false;
+      expect(created, true);
+    });
+
+  });
+}
+
