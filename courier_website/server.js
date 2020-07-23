a3ed22c6cb73ed0aded8a3a257bfce9b4f587d71
unknown <carlvonm10555@gmail.com>
2020-07-23 15:01:30 +0200

    Attempt to fix Express path

--- courier_website/server.js
+++ courier_website/server.js
@@ -4,12 +4,12 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(favicon(__dirname+'/public/favicon.ico'));
+app.use(favicon(__dirname+'/build/favicon.ico'));
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port);
