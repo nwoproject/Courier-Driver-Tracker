697db1f0c1400633190270115fd88d803acf66ab
Ryan <u15016880@tuks.co.za>
2020-08-07 15:42:05 +0200

    Adding file manager for native android, write functionality

---
+++ courier_driver_tracker/android/app/src/main/java/com/ctrlaltelite/courier_driver_tracker/FileManager.java
@@ -0,0 +1,36 @@
+package com.ctrlaltelite.courier_driver_tracker;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileManager {
+    public FileManager() {
+
+    }
+
+    public Boolean write(String fileName, String locDetails) {
+        try {
+            String path = "/sdcard/"+fileName+".txt";
+
+            File file = new File(path);
+
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(locDetails);
+            bw.close();
+            return true;
+        } catch (IOException e) {
+            e.printStrackTrace();
+            return false;
+        }
+    }
+}
+
--- courier_driver_tracker/test/Route_logging_test.dart
+++ courier_driver_tracker/test/Route_logging_test.dart
@@ -1,5 +1,6 @@
 import 'package:courier_driver_tracker/services/location/RouteLogging.dart';
 import 'package:flutter_test/flutter_test.dart';
+
 void main() {
   group('PathProvider', () {
     TestWidgetsFlutterBinding.ensureInitialized();
