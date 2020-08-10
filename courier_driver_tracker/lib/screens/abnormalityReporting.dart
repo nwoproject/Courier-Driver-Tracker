dbf6d9707eac7591a07a6170e5707402b0939445
Ryan <u15016880@tuks.co.za>
2020-08-10 13:49:20 +0200

    Adding feedback screen
    Previous uploads not adding the code and only the file

---
+++ courier_driver_tracker/lib/screens/abnormalityReporting.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+class UserFeedback extends StatelessWidget {
+  static const String _title = 'Abnormality Feedback';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: Center(
+          child: Feedback(),
+        ),
+      ),
+    );
+  }
+}
+
+enum Abnormality { fuelstop, lunch, traffic, other }
+
+class Feedback extends StatefulWidget {
+  Feedback({Key key}) : super(key: key);
+
+  @override
+  _FeedbackState createState() => _FeedbackState();
+}
+
+class _FeedbackState extends State<Feedback> {
+  Abnormality _character = Abnormality.fuelstop;
+  TextEditingController _controller;
+
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        RadioListTile(
+          title: const Text('Filling up vehicle'),
+
+          value: Abnormality.fuelstop,
+          groupValue: _character,
+          onChanged: (Abnormality value) {
+            print(value);
+            setState(() {
+              _character = value;
+            });
+          },
+          secondary: new Icon(Icons.add_circle),
+        ),
+        RadioListTile(
+          title: const Text('Stopped for lunch break'),
+
+          value: Abnormality.lunch,
+          groupValue: _character,
+          onChanged: (Abnormality value) {
+            print(value);
+            setState(() {
+              _character = value;
+            });
+          },
+          secondary: new Icon(Icons.add_circle),
+        ),
+
+        RadioListTile(
+          title: const Text('Severe traffic'),
+          value: Abnormality.traffic,
+          groupValue: _character,
+          onChanged: (Abnormality value) {
+            print(value);
+            setState(() {
+              _character = value;
+            });
+          },
+          secondary: new Icon(Icons.add_circle),
+        ),
+
+        RadioListTile(
+          title: const Text('Other (Specify)'),
+          value: Abnormality.other,
+          groupValue: _character,
+          onChanged: (Abnormality value) {
+            print(value);
+            setState(() {
+              _character = value;
+            });
+          },
+          secondary: new Icon(Icons.add_circle),
+        ),
+
+        TextField(
+          decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Specify reason'
+          ),
+        ),
+
+        const SizedBox(height: 30),
+        RaisedButton(
+          onPressed: () {},
+          child: const Text('Submit', style: TextStyle(fontSize: 20)),
+        ),
+      ],
+    );
+  }
+}
