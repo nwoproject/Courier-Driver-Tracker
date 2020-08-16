0d6ec1ed93e40ad740e31be7a2fbaccfc1806b08
Ryan <u15016880@tuks.co.za>
2020-08-16 12:52:45 +0200

    Native file manager created
    This will be used to store the users route when the application is running in the background.

--- courier_driver_tracker/android/app/src/main/java/com/ctrlaltelite/courier_driver_tracker/FileManager.java
+++ courier_driver_tracker/android/app/src/main/java/com/ctrlaltelite/courier_driver_tracker/FileManager.java
@@ -1,61 +1,69 @@
 package com.ctrlaltelite.courier_driver_tracker;
+import com.ctrlaltelite.courier_driver_tracker.location_service.*;
+
+import android.os.Environment;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.IOException;
 
+
 public class FileManager {
-    public FileManager() {
 
-    }
+    public void writeToFile(String data) {
+        File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File file = new File(dir,"test.txt");
 
-    public Boolean write(String fileName, String locDetails) {
         try {
-            String path = "/sdcard/"+fileName+".txt";
+            FileOutputStream outputStream = new FileOutputStream(file);
+            OutputStreamWriter oWriter = new OutputStreamWriter(outputStream);
 
-            File file = new File(path);
-
-            if (!file.exists()) {
+            if(!file.exists()){
                 file.createNewFile();
             }
 
-            FileWriter fw = new FileWriter(file.getAbsoluteFile());
-            BufferedWriter bw = new BufferedWriter(fw);
-            bw.write(locDetails);
-            bw.close();
-            return true;
-        } catch (IOException e) {
-            e.printStrackTrace();
-            return false;
+            oWriter.append(data);
+            oWriter.close();
+            outputStream.close();
+        }
+
+        catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    public String read(String fileName){
+    public String readFromFile() {
 
-        BufferedReader br = null;
-        String response = null;
+        StringBuffer stringBuffer = new StringBuffer();
+        String aRow = "";
+        String aBuffer = "";
+
+        File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File file = new File(dir,"test.txt");
+
+        if(!file.exists()){
+            return ("File does not exist.");
+        }
 
         try {
-
-            StringBuffer output = new StringBuffer();
-            String fpath = "/sdcard/"+fileName+".txt";
-
-            br = new BufferedReader(new FileReader(fpath));
-            String line = "";
-            while ((line = br.readLine()) != null) {
-                output.append(line +"n");
+            FileInputStream inputStream = new FileInputStream(file);
+            BufferedReader myReader = new BufferedReader(
+                    new InputStreamReader(inputStream)
+            );
+            while ((aRow = myReader.readLine()) !=null) {
+                aBuffer += aRow + "\n";
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-
+            myReader.close();
+            return aBuffer;
         }
-        return response;
-
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        return ("Trouble reading from file.");
     }
 }
 
