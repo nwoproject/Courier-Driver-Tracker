0d79c5b1631f341220b2c412d92f6cfcd6ffbb0b
GianGeyser <u14022096@tuks.co.za>
2020-08-20 18:56:11 +0200

    Fixed async problem with route_logging_test

--- courier_driver_tracker/test/Route_logging_test.dart
+++ courier_driver_tracker/test/Route_logging_test.dart
@@ -1,33 +1,32 @@
 import 'package:courier_driver_tracker/services/location/route_logging.dart';
 import 'package:flutter_test/flutter_test.dart';
+
 void main() {
-  group('PathProvider', () {
-    TestWidgetsFlutterBinding.ensureInitialized();
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  test('Local path should contain the path.', () async {
     RouteLogging logger = RouteLogging();
+    bool created = false;
+    String path = logger.localPath.toString();
+    if (path.length > 0) {
+      created = true;
+    }
+    else
+      created = false;
+    expect(created, true);
+  });
 
-    test('Local path should contain the path.', () async{
-      bool created = false;
-      String path = logger.localPath.toString();
-      if (path.length > 0)
-        {
-          created = true;
-        }
-      else
-        created = false;
-      expect(created, true);
-    });
-
-    test('Local file should contain the file.', () async*{
-      bool created = false;
-      String file = logger.locationFile.toString();
-      if (file.length > 0)
-      {
-        created = true;
-      }
-      else
-        created = false;
-      expect(created, true);
-    });
+  test('Local file should contain the file.', () async {
+    RouteLogging logger = RouteLogging();
+    bool created = false;
+    String file = logger.locationFile.toString();
+    if (file.length > 0) {
+      created = true;
+    }
+    else
+      created = false;
+    expect(created, true);
   });
 }
+
 
