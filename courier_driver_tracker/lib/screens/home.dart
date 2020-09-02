761838602e53fc01068ed091fd14cfdbf1ae6725
Jordan Nijs <54304244+Jordannijs@users.noreply.github.com>
2020-09-02 18:48:40 +0200

    API integration
    Change password screen added and integrated with API + additional bug fixes

--- courier_driver_tracker/lib/screens/home.dart
+++ courier_driver_tracker/lib/screens/home.dart
@@ -128,8 +128,6 @@ class _HomePageViewState extends State<HomePageView> {
             onTap: (index) {
               if (index == 0) {
                 Navigator.of(context).pushNamed("/delivery");
-              } else if (index == 1) {
-                Navigator.of(context).pushNamed("/home2");
               } else if (index == 2) {
                 Navigator.of(context).pushNamed("/profile");
               } else if (index == 3) {
