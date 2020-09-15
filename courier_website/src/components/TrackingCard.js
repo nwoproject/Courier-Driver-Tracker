22808a5ebec05a217f26ae6a531ec61842f0d486
unknown <carlvonm10555@gmail.com>
2020-09-15 12:32:51 +0200

    Added a dropdown for all Driver Selection

--- courier_website/src/components/TrackingCard.js
+++ courier_website/src/components/TrackingCard.js
@@ -1,7 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Card from 'react-bootstrap/Card';
-import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Spinner from 'react-bootstrap/Spinner';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import TrackMap from './TrackMap';
 
@@ -11,34 +15,67 @@ import './style/style.css';
 function TrackingCard(){
     const [DriverID, setID] = useState();
     const [Searched, setSearch] = useState(false);
-    
+    const [DriverName, setDN] = useState("None");
+    const [DriverList, setDL] = useState();
+    const [LoadingList, setLL] = useState(false);
+
+    useEffect(()=>{
+        fetch(process.env.REACT_APP_API_SERVER+"/api/reports/drivers",{
+            method: 'GET',
+            headers:{
+                'authorization': "Bearer "+process.env.REACT_APP_BEARER_TOKEN,
+                'Content-Type' : 'application/json'    
+            }
+        })
+        .then(response=>response.json())
+        .then(result=>{
+            setDL(result.drivers);
+            setLL(true);
+        });
+    },[]);
+
     function handleChange(event){
-        setID(event.target.value);
+        let Index = event.target.id;
+        setID(DriverList[Index].id);
+        setDN(DriverList[Index].name + " " + DriverList[Index].surname);
         setSearch(false);
     }
 
     function SubmitID(event){
-        event.preventDefault();   
-        setSearch(true);
+        if(DriverID!==""){
+            setSearch(true);
+        }    
     }
 
     return(
         <Card className="TrackingCard">
             <Card.Header>Always On Tracking</Card.Header>
             <Card.Body>
-                <Form inline onSubmit={SubmitID}>
-                    <Form.Label srOnly>
-                        Driver ID
-                    </Form.Label>
-                    <Form.Control 
-                        className="mb-2 mr-sm-2"
-                        id="DriverID"
-                        placeholder="Enter Driver ID to track"
-                        onChange={handleChange}/>
-                    <Button type="submit" className="mb-2">
-                        Submit
-                    </Button>
-                </Form>
+                {LoadingList ? 
+                <Row>
+                    <Col xs={1}>
+                        <DropdownButton 
+                            key="right"
+                            drop="right"
+                            title="Drivers">
+                            {DriverList.map((item,index)=>
+                                <Dropdown.Item name="DriverID" id={index} onClick={handleChange} key={index}>{item.name + " " + item.surname}</Dropdown.Item>
+                            )}     
+                        </DropdownButton>
+                    </Col>
+                    <Col xs={4}>
+                            <p>Currently Selected Driver : {DriverName}</p>
+                    </Col>
+                    <Col xs={2}>
+                        <Button onClick={SubmitID}>
+                            Submit
+                        </Button>
+                    </Col>
+                </Row>
+                :
+                <Spinner animation="border" role="status">
+                    <span className="sr-only">Loading...</span>
+                </Spinner>}
                 {Searched ? <TrackMap ID={DriverID}/> : null}
             </Card.Body>
         </Card>
