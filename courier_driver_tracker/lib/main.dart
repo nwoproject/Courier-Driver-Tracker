688f43e20e8305cb75cfd10053b7d9aa7d1eceb6
BrendanJvR <brendanjansenvanrensburg@gmail.com>
2020-09-16 00:57:18 +0200

    Keep phone from rotating screen as well as sleeping

--- courier_driver_tracker/lib/main.dart
+++ courier_driver_tracker/lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:courier_driver_tracker/routing.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:screen/screen.dart';
 
 Future main() async {
   await DotEnv().load('.env');
-  runApp(CourierDriverTracker());
+  Screen.keepOn(true);
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+  .then((_) {
+    runApp(CourierDriverTracker());
+  });
 }
 
 class CourierDriverTracker extends StatelessWidget {
