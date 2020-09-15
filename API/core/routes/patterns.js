b7d182e52de62cea5986c5eef6d330f9c35ea2b2
BrendanJvR <brendanjansenvanrensburg@gmail.com>
2020-09-15 23:48:15 +0200

    Added endpoint to log monthly pattern detection

--- API/core/routes/patterns.js
+++ API/core/routes/patterns.js
@@ -26,5 +26,28 @@ router.post('/weekly/:driverid', async (req,res)=>{
     }
 });
 
+router.post('/monthly/:driverid', async (req,res)=>{
+    const driverID =  req.params.driverid;
+    const datetime =  new Date(new Date() + 'GMT');
+    const patternDate = datetime.toISOString().slice(0,10);
+    
+    await checks.driverExistsCheck(driverID,res);
+
+    if(!res.writableEnded)
+    {
+        DB.pool.query('INSERT INTO ai."monthly_pattern"("pattern_detected","abnormality","week","date","driver_id")VALUES($1,$2,$3,$4,$5)',
+        [req.body.patternsDetected,req.body.abnormalities,req.body.weeks,patternDate,driverID],(err,results)=>{
+            if(err)
+            {
+                DB.dbErrorHandler(res,err);
+            }
+            else
+            {
+                res.status(200).end();
+            }
+        });
+    }
+});
+
 
 module.exports = router;
